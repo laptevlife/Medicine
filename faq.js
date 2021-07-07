476d5a58fb211440c1ce1e42026f0ecbd92a751f
Mikhail Laptev <56048517+laptevlife@users.noreply.github.com>
2021-07-07 16:33:55 +0400

    fix

--- faq.js
+++ faq.js
@@ -1,3 +1,58 @@
+// let innerHeight = window.innerHeight
+// const headerDT = document.querySelector('.headerDT')
+// const headerMobil = document.querySelector('.headerMobil')
+// const mobilMenu = document.querySelector('.mobil-menu')
+// const burger = document.querySelector('.burger')
+
+
+
+
+
+
+// window.addEventListener('resize', () => {
+//     innerWidth = window.innerWidth
+//     widthController()
+
+
+// })
+
+
+// const widthController = () => {
+//     if (innerWidth > 1340) {
+//         headerDT.setAttribute('style', 'display: block')
+//         headerMobil.setAttribute('style', 'display: none')
+//     }
+//     if (innerWidth < 1340) {
+//         headerDT.setAttribute('style', 'display: none')
+//         headerMobil.setAttribute('style', 'display: block')
+//     }
+// }
+// widthController()
+
+// let navState = false
+// burger.addEventListener('click', () => {
+//     navState = !navState
+//     if (navState) {
+//         // console.log(navState);
+
+//         burger.classList.add('burger-active')
+//         headerMobil.classList.add('header-mobil-active')
+
+//         // setTimeout( ()=> mobilMenu.setAttribute('style', 'display: block') , 200)
+//         setTimeout(() => hrefMobil.forEach(i => i.classList.add('visible')), 200)
+
+//     } else {
+//         // burger.classList.
+//         burger.classList.remove('burger-active')
+//         headerMobil.classList.remove('header-mobil-active')
+//         // mobilMenu.setAttribute('style', 'display: none')
+//         hrefMobil.forEach(i => i.classList.remove('visible'))
+//     }
+
+// })
+
+
+
 // let innerWidth = window.innerWidth
 let innerHeight = window.innerHeight
 let innerWidth = document.documentElement.clientWidth
@@ -17,30 +72,8 @@ const tooltip = document.querySelectorAll('.lkToolptip')
 
 
 // MODAL
-sponsorsButton.addEventListener('click', ()=>{
-    modal_wrap.classList.add('modal_wrap-active')
-})
-
-burger_modal.addEventListener('click', ()=>{
-    modal_wrap.classList.remove('modal_wrap-active')    
-})
 
 
-
-
-
-favoritesAdd.addEventListener('mouseover', ()=>{
-    console.log('enter');
-    butto__wrap.style.background= 'linear-gradient(125deg, #feda7a, #f72f2f, #7166cf)';
-    butto__wrap.style.backgroundSize =" 300% 220%"
-    butto__wrap.style.transition =" all 1s ease"
-})
-favoritesAdd.addEventListener('mouseout', ()=>{
-    console.log('enter2');
-    butto__wrap.style.background= 'linear-gradient(255deg, #feda7a, #f72f2f, #7166cf)';
-    butto__wrap.style.backgroundSize =" 220% 110%"
-    butto__wrap.style.transition =" all 1s ease"
-})
 
 
 
@@ -82,89 +115,29 @@ const tooltipController = () => {
 tooltipController()
 
 
-const leftArrow = document.querySelector('.arrowLeft') 
-const rightArrow = document.querySelector('.arrowRight') 
-const scrollContainer = document.querySelector('.scrollContainer') 
-var sectionIndex = 0;
-let specialtyItems = document.querySelectorAll('div[data-specialty]')//data-specialty
 
-//  let specialtyItems = document.querySelectorAll('.specialtyItem')
 
-const hideArrow = ()=>{
-    console.log('sectionIndex', sectionIndex);
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
     
-    if(  sectionIndex ===0   ) {
-        leftArrow.style.opacity = 0
-    }else{
-        leftArrow.style.opacity = 1  
-    }
-    if(  sectionIndex > specialtyItems.length-1   ) {
-        rightArrow.style.opacity = 0
-    }else{
-        rightArrow.style.opacity = 1  
-    }
+    const blockID = anchor.getAttribute('href').substr(1)
+    
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
 }
 
 
-hideArrow()
-
-leftArrow.addEventListener('click',function(){
-     sectionIndex <= 0 ? sectionIndex = 1 : null
-    scrollContainer.style.transform = 'translate(' + (sectionIndex -1) * -200 + 'px)';
-     sectionIndex = sectionIndex  - 1
-     hideArrow()
-    
-});
-
-
-rightArrow.addEventListener('click',function(){
-    // sectionIndex = (sectionIndex < 3) ? sectionIndex + 1:0;
-
-    // let specialtyItems = document.querySelectorAll('.specialtyItem')//data-specialty
-
-    if (sectionIndex === specialtyItems.length ) return
-    scrollContainer.style.transform = 'translate(' + (sectionIndex +1) * -200 + 'px)';
-     sectionIndex = sectionIndex +1
-     hideArrow()
-
-    
-});
-
-
-let cardsCount
-
-const countCards = function () {
-
-    if (innerWidth < 576) {
-        return cardsCount = 1.12
-    }
-    // if( innerWidth < 768 &&  innerWidth < 576    ){
-    if (innerWidth < 768) {
-        return cardsCount = 1.5
-    }
-    if (innerWidth < 992) {
-        return cardsCount = 2.2
-    }
-    if (innerWidth < 1340 && innerWidth > 992) {
-        return cardsCount = 3.1
-    }
-    if (innerWidth > 1340) {
-
-        return cardsCount = 4.1
-    }
-
-
-
-
-}
-
-countCards()
 
 window.addEventListener('resize', () => {
     innerWidth = window.innerWidth
     widthController()
     countCards()
-    
 
 })
 
@@ -211,7 +184,7 @@ var swiper = new Swiper(".mySwiper", {
     spaceBetween: 30,
 
     autoplay: {
-        delay: 5000,
+        delay: 2500,
         disableOnInteraction: false,
         pauseOnMouseEnter: true,
     },
@@ -230,12 +203,15 @@ var swiper2 = new Swiper(".banerSwiper", {
     spaceBetween: 30,
     slidesPerGroup: 1,
     autoplay: {
-        delay: 5000,
+        delay: 2500,
         disableOnInteraction: false,
         pauseOnMouseEnter: true,
     },
 
-
+    // navigation: {
+    //     nextEl: ".swiper-button-next",
+    //     prevEl: ".swiper-button-prev",
+    // },
 });
 var swiper3 = new Swiper(".cardsSwiper", {
 
@@ -249,18 +225,18 @@ var swiper3 = new Swiper(".cardsSwiper", {
         prevEl: ".swiper-button-prev",
     },
 });
-// var swiper3 = new Swiper(".specialtySwiper", {
+var swiper3 = new Swiper(".specialtySwiper", {
 
 
-//     slidesPerView: 5,
-//     spaceBetween: 30,
-//     slidesPerGroup: 1,
+    slidesPerView: 5,
+    spaceBetween: 30,
+    slidesPerGroup: 1,
 
-//     navigation: {
-//         nextEl: ".swiper-button-next",
-//         prevEl: ".swiper-button-prev",
-//     },
-// });
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
 
 // const slides = document.querySelectorAll('.swiper-slide')
 
